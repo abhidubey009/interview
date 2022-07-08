f0e0ef7b4151b557c3ebed8eaeecedf22916a612
abhidubey009 <dubey.abhishek009@gmail.com>
2022-07-08 15:58:22 -0500

    Update LongestCommonSubsequence.java
    Loop needs to run one more time

--- src/com/interview/dynamic/LongestCommonSubsequence.java
+++ src/com/interview/dynamic/LongestCommonSubsequence.java
@@ -22,8 +22,8 @@ public class LongestCommonSubsequence {
     
         int temp[][] = new int[str1.length + 1][str2.length + 1];
         int max = 0;
-        for(int i=1; i < temp.length; i++){
-            for(int j=1; j < temp[i].length; j++){
+        for(int i=1; i <= temp.length; i++){
+            for(int j=1; j <= temp[i].length; j++){
                 if(str1[i-1] == str2[j-1]) {
                     temp[i][j] = temp[i - 1][j - 1] + 1;
                 }
